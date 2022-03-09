2fbe5b0c9899b2902e49cf239eba93044a8db062
Miller Barros <mbarros@consumeraffairs.com>
2022-03-08 22:36:01 -0300

    feat: add Diagram component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import Diagram from './components/Diagram';
+
+import './App.scss';
 
 function App() {
+  // const chordC = [
+  //   { finger: 1, pos: 21 },
+  //   { finger: 2, pos: 42 },
+  //   { finger: 3, pos: 12 },
+  // ];
+  const chordC = [
+    { pos: 21 },
+    { pos: 42 },
+    { pos: 12 },
+  ];
+  const chordG = [
+    { pos: 21 },
+    { pos: 42 },
+    { pos: 53 },
+  ];
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Diagram stringType='CAVACO' chord={chordC} />
+      <Diagram stringType='GUITAR' chord={chordG} />
     </div>
   );
 }
